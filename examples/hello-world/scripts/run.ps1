006b7f8f35f1be3608a5fbf976f1a6443f4a8f3f
efokschaner <eddyaod@gmail.com>
2021-05-28 13:15:20 -0700

    Bump factorio version

--- examples/hello-world/scripts/run.ps1
+++ examples/hello-world/scripts/run.ps1
@@ -12,4 +12,4 @@ cp -r ../../mod/terraform-crud-api factorio-volume/mods
 mkdir factorio-volume/config
 Write-Output "SOMEPASSWORD" | Out-File -Encoding ASCII -NoNewLine factorio-volume/config/rconpw
 # Run factorio server
-docker run -it -p 127.0.0.1:34197:34197/udp -p 127.0.0.1:27015:27015/tcp -v "${PWD}/factorio-volume:/factorio" factoriotools/factorio:1.1.33
+docker run -it -p 127.0.0.1:34197:34197/udp -p 127.0.0.1:27015:27015/tcp -v "${PWD}/factorio-volume:/factorio" factoriotools/factorio:1.1.34
